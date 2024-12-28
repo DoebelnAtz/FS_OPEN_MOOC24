f0e4026289aecc612f5920bdaab27a7cef2802b9
DoebelnAtz <axel.ej.adlercreutz@gmail.com>
2024-12-28 12:15:17 +0200

    part 1.6-1.10

---
+++ part1/part6-11/src/App.js
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div className="App">
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text="good" />
+      <Button handleClick={handleNeutral} text="neutral" />
+      <Button handleClick={handleBad} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+}
+
+function Button(props) {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+function Statistics(props) {
+  const total = props.good + props.neutral + props.bad
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      {total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <StatisticsLine text="good" value={props.good} />
+          <StatisticsLine text="neutral" value={props.neutral} />
+          <StatisticsLine text="bad" value={props.bad} />
+          <StatisticsLine text="all" value={total} />
+          <StatisticsLine text="average" value={(props.good - props.bad) / total} />
+          <StatisticsLine text="positive" value={props.good / total * 100 + '%'} />
+        </>
+      )}
+    </div>
+  )
+}
+
+function StatisticsLine(props) {
+  return (
+    <p>{props.text} {props.value}</p>
+  )
+}
+
+export default App;
